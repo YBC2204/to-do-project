5f235d3b7174bb0f449ba2fbb41cd0483fa653c7
Yadhukrishnan <ybc2204@gmail.com>
2023-11-21 01:08:50 +0530

    95% finished

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="font-Poppins box-border  flex h-screen justify-center items-center text-center">
+      <TodoWrapper  />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+      // prevent default action
+        e.preventDefault();
+        if (value) {
+          // add todo
+          addTodo(value);
+          // clear form after submission
+          setValue('');
+        }
+      };
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm my-6 flex items-center">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input outline-none bg-gray-600 border border-[#3d3392] p-2  text-[#fff] w-10/12" placeholder="What is task for today?" />
+    <button type="submit" className='todo-btn bg-[#3d3392] text-white  border-none py-2 w-2/12'>Add</button>
+  </form>
+  )
+}
